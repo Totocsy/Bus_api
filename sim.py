39c4436f2746349cec75a2b41ea27840356b006c
Toth_Szilard <122917702+Totocsy@users.noreply.github.com>
2025-02-24 19:05:35 +0200

    Update sim.py

--- sim.py
+++ sim.py
@@ -38,4 +38,5 @@ def get_bus_movement(bus_id):
     return Response(generate_coordinates(), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
